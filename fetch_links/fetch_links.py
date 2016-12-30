1d41aeda549c621006c5563eba958ab86f8431d3
Joshua B. Smith <joshua.b.smith@us.ibm.com>
2016-12-30 11:53:32 -0500

    updated to be able to be deployed using openwhisk

--- fetch_links/fetch_links.py
+++ fetch_links/fetch_links.py
@@ -1,7 +1,25 @@
 import requests
 import xml.etree.ElementTree as ET
-r = requests.get('https://www.sec.gov/rss/investor/alerts')
-root = ET.fromstring(r.text)
-for link in [x.text for x in root.findall('.//channel/item/link')]:
-	print(link)
+import flask,json
 
+app = flask.Flask('Whisk Link Fetcher')
+
+@app.route('/init',methods=['POST'])
+def init():
+  return flask.Response(response='{}',
+                        status=200,
+                        mimetype='application/json')
+
+@app.route('/run', methods=['POST'])
+def main():
+  params = flask.request.get_json()
+  r = None
+  if 'url' in params:
+    r = requests.get(params['url'])
+  else:
+    r = requests.get('https://www.sec.gov/rss/investor/alerts')
+          
+  root = ET.fromstring(r.text)
+  return json.dumps([x.text
+                     for x
+                     in root.findall('.//channel/item/link')])
